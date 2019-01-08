63b5a62c54e30121ef768fbca5508c9f43259107
4thArmoredDev <46430843+4thArmoredDev@users.noreply.github.com>
2019-01-08 13:38:18 -0500

    Add code files

---
+++ spring1944-master/units/gbr/vehicles/GBRAECMkII.lua
@@ -0,0 +1,42 @@
+local GBRAECMkII = HeavyArmouredCar:New{
+	name				= "AEC Armoured Car Mk.II",
+	iconType            = "aecmkii",
+	buildCostMetal		= 1550,
+	maxDamage			= 1270,
+	trackOffset			= 10,
+	trackWidth			= 13,
+	turnRate			= 405,
+
+	weapons = {
+		[1] = {
+			name				= "qf6pdr57mmap",
+			mainDir				= [[0 16 1]],
+			maxAngleDif			= 210,
+		},
+		[2] = {
+			name				= "qf6pdr57mmhe",
+			mainDir				= [[0 16 1]],
+			maxAngleDif			= 210,
+		},
+		[3] = {
+			name				= "BESA",
+		},
+		[4] = {
+			name				= ".30calproof",
+		},
+	},
+	customParams = {
+		armor_front			= 57,
+		armor_rear			= 16,
+		armor_side			= 30,
+		armor_top			= 8,
+		maxammo				= 10,
+		turretturnspeed		= 32, -- 11s for 360
+		maxvelocitykmh		= 66,
+		normaltex			= "unittextures/GBRAECMkII_normals.dds",
+	}
+}
+
+return lowerkeys({
+	["GBRAECMkII"] = GBRAECMkII,
+})
