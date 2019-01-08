d5b45191ab924bc803af3e1a0deea47af941cdd1
4thArmoredDev <46430843+4thArmoredDev@users.noreply.github.com>
2019-01-08 13:53:34 -0500

    Add backups

---
+++ spring1944-master/icon_test_backups/GBRChurchillMkVII.lua
@@ -0,0 +1,47 @@
+local GBRChurchillMkVII = HeavyTank:New{
+	name				= "A22F Churchill Mk. VII",
+	description			= "Heavily Armoured All-Terrain Support Tank",
+	buildCostMetal		= 5800,
+	maxDamage			= 4060,
+	movementClass		= "TANK_Goat",
+	trackOffset			= 5,
+	trackWidth			= 18,
+
+	weapons = {
+		[1] = {
+			name				= "QF75mmAP",
+			mainDir				= [[0 16 1]],
+			maxAngleDif			= 210,
+		},
+		[2] = {
+			name				= "QF75mmHE",
+			mainDir				= [[0 16 1]],
+			maxAngleDif			= 210,
+		},
+		[3] = {
+			name				= "BESA",
+		},
+		[4] = {
+			name				= "BESA",
+			mainDir				= [[0 0 1]],
+			maxAngleDif			= 30,
+		},
+		[5] = {
+			name				= ".50calproof",
+		},
+	},
+	customParams = {
+		armor_front			= 148,
+		armor_rear			= 57,
+		armor_side			= 75,
+		armor_top			= 19,
+		maxammo				= 14,
+		turretturnspeed		= 24, -- 15s for 360
+		maxvelocitykmh		= 24,
+		normaltex			= "unittextures/GBRChurchillMkVII_normals.dds",
+	},
+}
+
+return lowerkeys({
+	["GBRChurchillMkVII"] = GBRChurchillMkVII,
+})
---
+++ spring1944-master/icon_test_backups/GBRCromwell.lua
@@ -0,0 +1,71 @@
+local GBRCromwell = MediumTank:New{
+	name				= "A27M Cromwell Mk. IV",
+	buildCostMetal		= 2680,
+	maxDamage			= 2800,
+	trackOffset			= 10,
+	trackWidth			= 18,
+
+	weapons = {
+		[1] = {
+			name				= "QF75mmAP",
+			mainDir				= [[0 16 1]],
+			maxAngleDif			= 210,
+		},
+		[2] = {
+			name				= "QF75mmHE",
+			mainDir				= [[0 16 1]],
+			maxAngleDif			= 210,
+		},
+		[3] = {
+			name				= "BESA",
+		},
+		[4] = {
+			name				= "BESA",
+			mainDir				= [[0 0 1]],
+			maxAngleDif			= 30,
+		},
+		[5] = {
+			name				= ".50calproof",
+		},
+	},
+	customParams = {
+		armor_front			= 71,
+		armor_rear			= 37,
+		armor_side			= 37,
+		armor_top			= 20,
+		maxammo				= 12,
+		turretturnspeed		= 25, -- 14-15s for 360
+		maxvelocitykmh		= 64,
+		normaltex			= "unittextures/GBRCromwell_normals.dds",
+	},
+}
+
+local GBRCromwellMkVI = GBRCromwell:New{
+	name				= "A27M Cromwell Mk. VI",
+	description			= "Close Support Tank",
+	
+		weapons = {
+		[1] = {
+			name				= "CS95mmHE",
+			mainDir				= [[0 16 1]],
+			maxAngleDif			= 210,
+		},
+		[2] = {
+			name				= "CS95mmSmoke",
+			mainDir				= [[0 16 1]],
+			maxAngleDif			= 210,
+		},
+		-- 3 and 4 should be provided by GBRCromwell base
+	},
+	customParams = {
+		maxammo				= 10,
+		weapontoggle		= "smoke",
+		canfiresmoke		= true,
+		normaltex		= "unittextures/GBRCromwellMkVI_normals.dds",
+	},
+}
+
+return lowerkeys({
+	["GBRCromwell"] = GBRCromwell,
+	["GBRCromwellMkVI"] = GBRCromwellMkVI,
+})
